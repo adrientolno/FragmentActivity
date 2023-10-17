4adfac9a221bba574a90928aab1b30aadfde293c
Adrien Tolno <tus29878@temple.edu>
2023-10-17 15:23:28 -0400

    Initial commit

---
+++ app/src/main/java/edu/temple/fragmentactivity/MainActivity.kt
@@ -0,0 +1,14 @@
+package edu.temple.fragmentactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().add(com.google.android.material.R.id.container, ColorSelectionFragment()).commit()
+
+    }
+}
